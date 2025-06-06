8ba1763f019f409ec143877fab024e9e538097e2
YossefHawela <138378304+YossefHawela@users.noreply.github.com>
2025-06-06 10:08:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,18 +6,77 @@ using Jaggy_Epub_Translator.Modules.Archive;
 using Jaggy_Epub_Translator.Modules.Epub;
 using Jaggy_Epub_Translator.Modules.Html;
 
-
-if (args.Length < 3)
+if (args.Length < 4)
 {
-    Console.WriteLine("Usage: <program> <filePath> <sourceLang> <targetLang>");
+    Console.WriteLine("Usage: <program> <filePath> <sourceLang> <targetLang> <outputPath>");
     return;
 }
 
-string path = args[0];
+string filePath = args[0];
+
 Languages sourceLanguage = Enum.Parse<Languages>(args[1], true);
 Languages targetLanguage = Enum.Parse<Languages>(args[2], true);
 
-EpubReader epubReader = new EpubReader(path);
+string outputPath = args[3];
+
+if (!File.Exists(filePath))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"ERROR: Input file '{filePath}' does not exist.");
+    Console.ResetColor();
+    return;
+}
+
+string outputDir;
+string inputFileNameWithoutExt = Path.GetFileNameWithoutExtension(filePath);
+string inputFileExt = Path.GetExtension(filePath);
+
+if (string.IsNullOrWhiteSpace(outputPath))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("ERROR: Output path is empty. Process aborted.");
+    Console.ResetColor();
+    return;
+}
+
+if (Path.HasExtension(outputPath))
+{
+    outputDir = Path.GetDirectoryName(outputPath)!;
+    if (string.IsNullOrEmpty(outputDir))
+    {
+        outputDir = Directory.GetCurrentDirectory();
+        outputPath = Path.Combine(outputDir, outputPath);
+    }
+}
+else
+{
+    outputDir = outputPath;
+    if (!outputDir.EndsWith(Path.DirectorySeparatorChar.ToString()))
+        outputDir += Path.DirectorySeparatorChar;
+
+    if (!Directory.Exists(outputDir))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("WARNING: Output directory does not exist. Process aborted.");
+        Console.ResetColor();
+        return;
+    }
+
+    string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+    string defaultFileName = $"{inputFileNameWithoutExt}_{timestamp}{inputFileExt}";
+    outputPath = Path.Combine(outputDir, defaultFileName);
+}
+
+if (!Directory.Exists(outputDir))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("WARNING: Output directory does not exist. Process aborted.");
+    Console.ResetColor();
+    return;
+}
+
+
+EpubReader epubReader = new EpubReader(filePath);
 
 await epubReader.ReadEpubAsync(p => Console.Write($"\rReading file: {p}%"));
 Console.WriteLine();
@@ -56,7 +115,7 @@ for (int i = 0; i < TranslatedDocuments.Length; i++)
 
 var ArchiveBytes = ArchiveTools.CreateZipArchive(AllFiles);
 
-string targetPath = Path.Combine(Path.GetDirectoryName(path)!, "Translated.epub");
+string targetPath = outputPath;
 
 File.WriteAllBytes(targetPath,ArchiveBytes);
 
